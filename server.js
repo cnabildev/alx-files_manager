561a6a3531e43d2c005eb9fee651fb9147dc3fea
CHERRADI NABIL Mouhssine <46906074+cnabildev@users.noreply.github.com>
2024-12-09 14:47:22 +0100

    fix

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-#!/usr/bin/node
 import express from 'express';
-import injectRoutes from './routes';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-injectRoutes(app);
+app.use(express.json());
 
-app.use(express.json({ limit: '200mb' }));
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
